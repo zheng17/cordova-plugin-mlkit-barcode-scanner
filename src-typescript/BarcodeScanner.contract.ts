548059c51cbd2d8653e0f75e8a8c275dbed7dccb
Zheng <luzhengno1@gmail.com>
2022-02-22 01:07:54 -0600

    build plugins
    build plugins

--- src-typescript/BarcodeScanner.contract.ts
+++ src-typescript/BarcodeScanner.contract.ts
@@ -1,7 +1,6 @@
 import { MLKitBarcodeScanner } from './BarcodeScanner.plugin';
 
 declare global {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   interface CordovaPlugins {
     mlkit: {
       barcodeScanner: MLKitBarcodeScanner;
